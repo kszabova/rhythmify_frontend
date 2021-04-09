3559be73af885c1f0b10ef72f7a4e93c34b801fa
kristina <k.szabova98@gmail.com>
2021-04-09 23:31:09 +0200

    Mock bar chart

--- src/app/components/histogram/histogram.component.ts
+++ src/app/components/histogram/histogram.component.ts
@@ -9,7 +9,7 @@ import * as d3 from 'd3';
 })
 export class HistogramComponent implements OnInit {
 
-  hist_data: any;
+  hist_data = [];
   private svg;
   private margin = 50;
   private width = 750 - (this.margin * 2);
@@ -20,57 +20,57 @@ export class HistogramComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.chantService.findByIncipit("Ab occultis").subscribe(
-      (data:(any)) => this.hist_data = data.map(chant => ({"incipit": chant.incipit,
-                                                          "vol_length": chant.volpiano.length}))
+    this.chantService.findByIncipit("Aetern").subscribe(
+      (data:(any)) => {
+        this.hist_data = data.map(chant => ({"incipit": chant.incipit, "vol_length": chant.volpiano.length}));
+        this.createSvg();
+        this.drawBars(this.hist_data);
+      }
     );
-    this.createSvg();
-    this.drawBars(this.hist_data);
   }
 
   private createSvg(): void {
-    this.svg = d3.select("figure#histogram")
+    this.svg = d3.select("figure#hist")
     .append("svg")
     .attr("width", this.width + (this.margin * 2))
     .attr("height", this.height + (this.margin * 2))
     .append("g")
     .attr("transform", "translate(" + this.margin + "," + this.margin + ")");
-}
+  }
 
-private drawBars(data: any[]): void {
-  // Create the X-axis band scale
-  const x = d3.scaleBand()
-  .range([0, this.width])
-  .domain(data.map(d => d.incipit))
-  .padding(0.2);
+  private drawBars(data: any[]): void {
+    // Create the X-axis band scale
+    const x = d3.scaleBand()
+    .range([0, this.width])
+    .domain(data.map(d => d.incipit))
+    .padding(0.2);
 
-  // Draw the X-axis on the DOM
-  this.svg.append("g")
-  .attr("transform", "translate(0," + this.height + ")")
-  .call(d3.axisBottom(x))
-  .selectAll("text")
-  .attr("transform", "translate(-10,0)rotate(-45)")
-  .style("text-anchor", "end");
+    // Draw the X-axis on the DOM
+    this.svg.append("g")
+    .attr("transform", "translate(0," + this.height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
 
-  // Create the Y-axis band scale
-  const y = d3.scaleLinear()
-  .domain([0, 200000])
-  .range([this.height, 0]);
+    // Create the Y-axis band scale
+    const y = d3.scaleLinear()
+    .domain([0, d3.max(data, (d) => d.vol_length)])
+    .range([this.height, 0]);
 
-  // Draw the Y-axis on the DOM
-  this.svg.append("g")
-  .call(d3.axisLeft(y));
+    // Draw the Y-axis on the DOM
+    this.svg.append("g")
+    .call(d3.axisLeft(y));
 
-  // Create and fill the bars
-  this.svg.selectAll("bars")
-  .data(data)
-  .enter()
-  .append("rect")
-  .attr("x", d => x(d.incipit))
-  .attr("y", d => y(d.vol_length))
-  .attr("width", x.bandwidth())
-  .attr("height", (d) => this.height - y(d.vol_length))
-  .attr("fill", "#d04a35");
-}
-
+    // Create and fill the bars
+    this.svg.selectAll("bars")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", d => x(d.incipit))
+    .attr("y", d => y(d.vol_length))
+    .attr("width", x.bandwidth())
+    .attr("height", (d) => this.height - y(d.vol_length))
+    .attr("fill", "#d04a35");
+  }
 }
